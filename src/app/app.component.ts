5cfd0e459609e49865193ff8fc45c76f018004b8
gregclar2019 <daddygreggy09@gmail.com>
2023-06-03 08:21:17 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rcp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'wdd-430-recipes';
+}
