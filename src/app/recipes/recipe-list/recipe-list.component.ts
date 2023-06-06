1b88280cf4533358554701e76d9fea8ae41837af
gregclar2019 <daddygreggy09@gmail.com>
2023-06-06 04:56:06 -0400

    adding components -greg

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'rcp-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'this is simply a test', '../../assets/images/recipe1.jpg'),
+    new Recipe('A test recipe', 'this is simply a test', '../../assets/images/recipe1.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,15 @@
+// this is just using vanilla JS
+// this will serve as a blueprint for each recipe that we create
+
+export class Recipe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, description: string, imagePath: string) {
+    this.name = name;
+    this.description = description;
+    this.imagePath = imagePath;
+  }
+
+}
